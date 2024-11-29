20bcca97d7b32dcd15ad99c2bcebcea35d220ba3
8srael <kouassiisrael36@gmail.com>
2024-11-29 22:54:49 +0000

    task 0: sql file updated. added order_by

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,7 +3,7 @@
 SELECT * FROM bookings INNER JOIN users on bookings.user_id == users.user_id
 
 --- Query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews
-SELECT * FROM properties LEFT JOIN reviews on properties.property_id == reviews.property_id
+SELECT * FROM properties LEFT JOIN reviews on properties.property_id == reviews.property_id ORDER BY properties.property_id
 
 --- Query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT * FROM users FULL OUTER JOIN bookings on users.user_id == bookings.user_id
